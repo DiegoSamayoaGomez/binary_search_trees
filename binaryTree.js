69c16cf3ec8ea90131e55f2080c75f4034bacfbc
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-06-25 21:12:41 -0600

    Create an array with unique values and sorted

--- binaryTree.js
+++ binaryTree.js
@@ -8,13 +8,17 @@ function Tree(myArr, root) {}
 
 function buildTree(myArr) {}
 
+// Example ARRAY
 const myArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-const xd = myArr.sort((a, b) => {
+// Take an array and sort it numerically
+const sortAndRemoveDuplicates = myArr.sort((a, b) => {
   return a - b;
 });
 
-let s = new Set(xd);
+// Create an object with unique values
+let s = new Set(sortAndRemoveDuplicates);
 
+// Create a new array with sorted and unique values
 let myNewArr = [...s];
 console.log(myNewArr);
