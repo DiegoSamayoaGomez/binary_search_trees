0f173f831241528574ae85fb8a391bd5ffd33770
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-07-08 22:22:26 -0600

    Return a sorted array without repeated elements

--- binaryTree.js
+++ binaryTree.js
@@ -2,3 +2,19 @@
 function Node(value, left = null, right = null) {
   return { value, left, right };
 }
+
+// Tree factory which accepts an array
+function Tree(myArr, root) {}
+
+function buildTree(myArr) {}
+
+const myArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const xd = myArr.sort((a, b) => {
+  return a - b;
+});
+
+let s = new Set(xd);
+
+let myNewArr = [...s];
+console.log(myNewArr);
